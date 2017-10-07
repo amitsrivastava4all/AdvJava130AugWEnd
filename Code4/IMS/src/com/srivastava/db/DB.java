c52e43c5a67966b23b308a93fa956cc353429559
amit4alljava <amit4alljava@gmail.com>
2017-10-07 15:25:13 +0530

    Codes

---
+++ Code4/IMS/src/com/srivastava/db/DB.java
@@ -0,0 +1,95 @@
+package com.srivastava.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.srivastava.dto.ProductDTO;
+
+public class DB {
+	private Connection getConnection() throws ClassNotFoundException, SQLException{
+		ResourceBundle rb = ResourceBundle.getBundle("db");
+		Class.forName(rb.getString("drivername"));
+		Connection con = DriverManager.getConnection(rb.getString("url"),rb.getString("userid"),"");
+		return con;
+	}
+	
+	public ArrayList<ProductDTO> getProductsByCriteria(String status, String search) throws SQLException, ClassNotFoundException{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<ProductDTO> productList = new ArrayList<>();
+		final String SQL = "select id, name, descr, price, status, image from product where status=? and price>=?";
+		try{
+			con = getConnection();
+			pstmt = con.prepareStatement(SQL);
+			pstmt.setString(1, status);
+			pstmt.setDouble(2, Double.parseDouble(search));
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				ProductDTO product = new ProductDTO();
+				product.setId(rs.getInt("id"));
+				product.setName(rs.getString("name"));
+				product.setDescr(rs.getString("descr"));
+				product.setPrice(rs.getDouble("price"));
+				product.setStatus(rs.getString("status"));
+				product.setImage(rs.getString("image"));
+				productList.add(product);
+			}
+			
+		}
+		finally{
+			if(rs!=null){
+				rs.close();
+			}
+			if(pstmt!=null){
+				pstmt.close();
+			}
+			if(con!=null){
+				con.close();
+			}
+		}
+		return productList;
+	}
+	
+	public ArrayList<ProductDTO> getProducts(String status) throws ClassNotFoundException, SQLException{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<ProductDTO> productList = new ArrayList<>();
+		final String SQL = "select id, name, descr, price, status, image from product where status=?";
+		try{
+			con = getConnection();
+			pstmt = con.prepareStatement(SQL);
+			pstmt.setString(1, status);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				ProductDTO product = new ProductDTO();
+				product.setId(rs.getInt("id"));
+				product.setName(rs.getString("name"));
+				product.setDescr(rs.getString("descr"));
+				product.setPrice(rs.getDouble("price"));
+				product.setStatus(rs.getString("status"));
+				product.setImage(rs.getString("image"));
+				productList.add(product);
+			}
+			
+		}
+		finally{
+			if(rs!=null){
+				rs.close();
+			}
+			if(pstmt!=null){
+				pstmt.close();
+			}
+			if(con!=null){
+				con.close();
+			}
+		}
+		return productList;
+	}
+}
