081448f5e5519ff827bd54924ccded9c60e7f413
amit4alljava <amit4alljava@gmail.com>
2017-10-08 15:39:12 +0530

    Codes

---
+++ Code5/IMS/src/com/srivastava/dto/ProductDTO.java
@@ -0,0 +1,48 @@
+package com.srivastava.dto;
+
+public class ProductDTO {
+	private int id;
+	private String name;
+	private String descr;
+	private String image;
+	private double price;
+	private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
